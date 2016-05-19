0f066d58849d4a3f2520ea41c7c1706a40581fa8
tschaef <tschaef1@gwdg.de>
2016-05-19 16:50:41 +0200

    Add expand/collapse button to facets
    If facet list contains more than 6 items, show only the first 5 with
    an expand button. On collapse, keep scroll position relative to button.

---
+++ app/Resources/js/facets.js
@@ -0,0 +1,21 @@
+(function() {
+	$('article.facet').each( function() {
+		if ($(this).find('.facetList').children().length < 7) {
+			$(this).find('.facetListExpand').hide();
+		}
+	});
+
+	$('.facetListExpand, .facetListCollapse').click( function() {
+		var $facetList = $(this).siblings('.facetList');
+
+		// Scroll viewport so toggle button's vertical position doesn't change
+		if ($(this).hasClass('facetListCollapse')) {
+			expandedListHeight = $facetList.offset().top + $facetList.height() - $facetList.find('li:nth-child(6)').offset().top;
+			scrollPos = $('body').scrollTop() - expandedListHeight;
+			$('html, body').animate({scrollTop: scrollPos});
+		}
+
+		$facetList.find('li:nth-child(n+6)').slideToggle();
+		$(this).siblings('.facetListExpand, .facetListCollapse').addBack().toggle();
+	});
+})();
