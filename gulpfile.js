4e29e17b1a928827526b557c8b6b892a6eb29deb
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2016-05-19 13:31:30 +0200

    Fix font-copying in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -13,54 +13,54 @@ var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync').create();
 
 var paths = {
-	appFiles: 'app/Resources/**/*.{md,php,yml}',
-	proxy: 'localhost:8000',
-	serveDir: '/',
+    appFiles: 'app/Resources/**/*.{md,php,yml}',
+    proxy: 'localhost:8000',
+    serveDir: '/',
 
-	fontsSrc: ['node_module/open-sans-fontface/fonts/**'],
-	fontsDest: 'web/fonts',
-	scriptSrc: 'app/Resources/js/**/*.js',
-	scriptDest: 'web/js',
-	styleSrc: 'app/Resources/scss/**/*.scss',
-	styleDest: 'web/css',
+    fontsSrc: 'node_modules/open-sans-fontface/fonts/**',
+    fontsDest: 'web/fonts/open-sans',
+    scriptSrc: 'app/Resources/js/**/*.js',
+    scriptDest: 'web/js',
+    styleSrc: 'app/Resources/scss/**/*.scss',
+    styleDest: 'web/css'
 };
 
-gulp.task('fonts', function() {
+gulp.task('fonts', function () {
     gulp.src(paths.fontsSrc)
         .pipe(gulp.dest(paths.fontsDest));
 });
 
-gulp.task('scripts', function() {
-	gulp.src(paths.scriptSrc)
-		.pipe(newer(paths.scriptDest + '/*.js'))
-		.pipe(filter(paths.scriptSrc))
-		.pipe(concat('script.js'))
-		.pipe(uglify())
-		.on('error', notify.onError({title: 'Uglify Error', message: '<%=error%>'}))
-		.pipe(gulp.dest(paths.scriptDest))
-		.pipe(browserSync.stream());
+gulp.task('scripts', function () {
+    gulp.src(paths.scriptSrc)
+        .pipe(newer(paths.scriptDest + '/*.js'))
+        .pipe(filter(paths.scriptSrc))
+        .pipe(concat('script.js'))
+        .pipe(uglify())
+        .on('error', notify.onError({title: 'Uglify Error', message: '<%=error%>'}))
+        .pipe(gulp.dest(paths.scriptDest))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('styles', function() {
-	gulp.src(paths.styleSrc)
-		.pipe(sassLint())
-		.pipe(newer(paths.styleDest + '/*.css'))
-		.pipe(filter(paths.styleSrc))
-		.pipe(sassGlob())
-		.pipe(sass())
-		.on('error', notify.onError({title: 'Sass Error', message: '<%=error%>'}))
-		.pipe(cssnano())
-		.pipe(autoprefixer())
-		.pipe(gulp.dest(paths.styleDest))
-		.pipe(browserSync.stream());
+gulp.task('styles', function () {
+    gulp.src(paths.styleSrc)
+        .pipe(sassLint())
+        .pipe(newer(paths.styleDest + '/*.css'))
+        .pipe(filter(paths.styleSrc))
+        .pipe(sassGlob())
+        .pipe(sass())
+        .on('error', notify.onError({title: 'Sass Error', message: '<%=error%>'}))
+        .pipe(cssnano())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(paths.styleDest))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('compile', ['fonts', 'scripts', 'styles']);
 
-gulp.task('default', ['scripts', 'styles'], function() {
-	browserSync.init({open: false, proxy: paths.proxy});
-	gulp.watch(paths.appFiles).on('change', browserSync.reload);
-	gulp.watch(paths.fontsSrc, ['fonts']);
-	gulp.watch(paths.scriptSrc, ['scripts']);
-	gulp.watch(paths.styleSrc, ['styles']);
+gulp.task('default', ['scripts', 'styles'], function () {
+    browserSync.init({open: false, proxy: paths.proxy});
+    gulp.watch(paths.appFiles).on('change', browserSync.reload);
+    gulp.watch(paths.fontsSrc, ['fonts']);
+    gulp.watch(paths.scriptSrc, ['scripts']);
+    gulp.watch(paths.styleSrc, ['styles']);
 });
