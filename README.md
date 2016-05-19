4aa19963f3ef853b6f5f1a9c36d9a58e146a2b1f
tschaef <tschaef1@gwdg.de>
2016-05-19 13:00:29 +0200

    Add build script
    - Add package.json and gulpfile.js with build tasks
    - Change directories to lowercase where allowed
    - Add old theme (work in progress, but compileable)

--- README.md
+++ README.md
@@ -3,8 +3,16 @@
 
 ## Requirements
 
-see http://symfony.com/doc/current/reference/requirements.html
+You need PHP 7 with cURL and DOMDocument installed (`apt-get install php-curl php-xml`).
+
+Also see http://symfony.com/doc/current/reference/requirements.html.
 
 ## Installation
 
-Install all dependencies with ```composer install``` and start the application with ```php bin/console server:start```
+Install backend dependencies with `composer install`.
+
+Install frontend dependencies with `npm install`, then run `gulp compile`.
+
+## Running locally
+
+Start the application with `php bin/console server:start`.
---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var cssnano = require('gulp-cssnano');
+var filter = require('gulp-filter');
+var newer = require('gulp-newer');
+var notify = require('gulp-notify');
+var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
+var sassLint = require('gulp-sass-lint');
+var uglify = require('gulp-uglify');
+
+var browserSync = require('browser-sync').create();
+
+var paths = {
+	appFiles: 'app/Resources/**/*.{md,php,yml}',
+	proxy: 'localhost:8000',
+	serveDir: '/',
+
+	fontsSrc: ['node_module/open-sans-fontface/fonts/**'],
+	fontsDest: 'web/fonts',
+	scriptSrc: 'app/Resources/js/**/*.js',
+	scriptDest: 'web/js',
+	styleSrc: 'app/Resources/scss/**/*.scss',
+	styleDest: 'web/css',
+};
+
+gulp.task('fonts', function() {
+    gulp.src(paths.fontsSrc)
+        .pipe(gulp.dest(paths.fontsDest));
+});
+
+gulp.task('scripts', function() {
+	gulp.src(paths.scriptSrc)
+		.pipe(newer(paths.scriptDest + '/*.js'))
+		.pipe(filter(paths.scriptSrc))
+		.pipe(concat('script.js'))
+		.pipe(uglify())
+		.on('error', notify.onError({title: 'Uglify Error', message: '<%=error%>'}))
+		.pipe(gulp.dest(paths.scriptDest))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('styles', function() {
+	gulp.src(paths.styleSrc)
+		.pipe(sassLint())
+		.pipe(newer(paths.styleDest + '/*.css'))
+		.pipe(filter(paths.styleSrc))
+		.pipe(sassGlob())
+		.pipe(sass())
+		.on('error', notify.onError({title: 'Sass Error', message: '<%=error%>'}))
+		.pipe(cssnano())
+		.pipe(autoprefixer())
+		.pipe(gulp.dest(paths.styleDest))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('compile', ['fonts', 'scripts', 'styles']);
+
+gulp.task('default', ['scripts', 'styles'], function() {
+	browserSync.init({open: false, proxy: paths.proxy});
+	gulp.watch(paths.appFiles).on('change', browserSync.reload);
+	gulp.watch(paths.fontsSrc, ['fonts']);
+	gulp.watch(paths.scriptSrc, ['scripts']);
+	gulp.watch(paths.styleSrc, ['styles']);
+});
