b9f7136e12dfe6d184bb7c7a611ed5d1c9a91cda
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2016-05-30 10:03:58 +0200

    Add just-scanned to colophon
    This adds the last scanned document into the footer.

--- src/AppBundle/Controller/DocumentController.php
+++ src/AppBundle/Controller/DocumentController.php
@@ -27,6 +27,7 @@ class DocumentController extends Controller
             ->children()
             ->each(function (Crawler $node, $i) {
                 $toc = new TableOfContents();
+
                 $toc->setId($node->attr('ID'));
                 $toc->setType($node->attr('TYPE'));
                 $toc->setDmdid($node->attr('DMDID'));
@@ -79,5 +80,20 @@ class DocumentController extends Controller
         );
 
         return $response;
+    }
+
+    /**
+     * @Route("/just-scanned/", name="just-scanned")
+     */
+    public function justScannedAction()
+    {
+        $client = $this->get('solarium.client');
+        $select = $client->createSelect();
+        $select->setRows(1);
+        $select->addSort('dateindexed', 'desc');
+
+        return $this->render('partials/app/just-scanned.html.twig', [
+            'document' => $client->select($select)->getDocuments()[0],
+        ]);
     }
 }
