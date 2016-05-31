fc4b1d497619097ab578d6d0a7a27d6a00e11e81
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2016-05-31 10:30:12 +0200

    Test if dom element exists before applying functions to it

--- app/Resources/js/coverflow.js
+++ app/Resources/js/coverflow.js
@@ -1,14 +1,17 @@
 $(function () {
 	var $coverflow = $('#coverflow');
 
-	$('img').lazyload({
-		container: $coverflow,
-		effect: 'fadeIn',
-	});
+	if ($coverflow.length > 0) {
 
-	var currentPos = $coverflow.find('.coverflow_link.-current').offset().left - $coverflow.width() / 2.5;
-	$coverflow.scrollLeft(currentPos).mousewheel( function(event, delta) {
-		this.scrollLeft -= (delta * 100);
-		event.preventDefault();
-	});
+		$('img').lazyload({
+			container: $coverflow,
+			effect: 'fadeIn',
+		});
+
+		var currentPos = $coverflow.find('.coverflow_link.-current').offset().left - $coverflow.width() / 2.5;
+		$coverflow.scrollLeft(currentPos).mousewheel( function(event, delta) {
+			this.scrollLeft -= (delta * 100);
+			event.preventDefault();
+		});
+	}
 });
