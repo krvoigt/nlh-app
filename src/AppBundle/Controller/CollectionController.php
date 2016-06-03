b69f2c923d09f25773ef1342684df7446ec1c225
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2016-06-03 10:27:21 +0200

    Optimize collection building
    This adds an extra template for facets in collections - but inherits the
    default one.
    Some methods from the CollectionController have been extracted to separate
    methods to gain a better modularity.

--- src/AppBundle/Controller/CollectionController.php
+++ src/AppBundle/Controller/CollectionController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Solarium\QueryType\Select\Query\Component\FacetSet;
 use Solarium\QueryType\Select\Query\FilterQuery;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,51 +22,28 @@ class CollectionController extends Controller
 
     /**
      * @Route("/collection/{id}", name="_collection", methods={"GET"})
-     *
-     * @todo avoid code duplication!
      */
     public function collectionAction(Request $request, $id)
     {
         $client = $this->get('solarium.client');
-        $select = $client->createSelect();
-        $query = $request->get('q') ?: '';
-        $facetConfiguration = $this->getParameter('facets');
-
         $paginator = $this->get('knp_paginator');
+        $activeFacets = $request->get('filter');
+
+        $query = $request->get('q') ?: '';
+        $currentPage = (int) $request->get('page') ?: 1;
+
         $rows = (int) $this->getParameter('results_per_page');
 
-        $filterQuery = new FilterQuery();
-        $filterQuery->setKey('dc');
-        $filterQuery->setQuery('dc:'.$id);
-
-        $workQuery = new FilterQuery();
-        $workQuery->setKey('work');
-        $workQuery->setQuery('iswork:true');
-
-        $select->addFilterQueries([$filterQuery, $workQuery]);
-
-        $currentPage = (int) $request->get('page') ?: 1;
         $offset = ($currentPage - 1) * $rows;
+
+        $select = $client->createSelect();
 
         $facetSet = $select->getFacetSet();
 
-        foreach ($facetConfiguration as $facet) {
-            $facetSet->createFacetField($facet['title'])->setField($facet['field']);
-        }
+        $filters = array_merge($this->addFacets($facetSet, $activeFacets), $this->getDefaultFilters($id));
 
-        $activeFacets = $request->get('filter');
-        $facetCounter = count($activeFacets) ?: 0;
-
-        if (count($activeFacets) > 0) {
-            foreach ($activeFacets as $activeFacet) {
-                $filterQuery = new FilterQuery();
-                foreach ($activeFacet as $itemKey => $item) {
-                    $filterQuery->setKey($itemKey.$facetCounter);
-                    $filterQuery->setQuery($itemKey.':"'.$item.'"');
-                }
-                $select->addFilterQuery($filterQuery);
-                ++$facetCounter;
-            }
+        foreach ($filters as $filter) {
+            $select->addFilterQuery($filter);
         }
 
         $results = $client->select($select);
@@ -79,13 +57,72 @@ class CollectionController extends Controller
             $rows
         );
 
-        return $this->render('@SubugoeFind/Default/index.html.twig', [
+        return $this->render('@SubugoeFind/Default/collections.html.twig', [
             'pagination' => $pagination,
             'query' => $query,
             'facets' => $results->getFacetSet()->getFacets(),
-            'facetCounter' => $facetCounter,
+            'facetCounter' => $this->getFacetCounter($request),
             'queryParams' => $request->get('filter') ?: [],
             'offset' => $offset,
         ]);
+    }
+
+    /**
+     * @param string $id
+     *
+     * @return array
+     */
+    protected function getDefaultFilters($id)
+    {
+        $filterQueries = [];
+
+        $dcFilter = new FilterQuery();
+        $dcFilter->setKey('dc')->setQuery('dc:'.$id);
+
+        $workQuery = new FilterQuery();
+        $workQuery->setKey('work')->setQuery('iswork:true');
+
+        $filterQueries[] = $dcFilter;
+        $filterQueries[] = $workQuery;
+
+        return $filterQueries;
+    }
+
+    protected function getFacetCounter($activeFacets)
+    {
+        $facetCounter = count($activeFacets) ?: 0;
+
+        return $facetCounter;
+    }
+
+    /**
+     * @param FacetSet $facetSet
+     * @param array    $activeFacets
+     *
+     * @return array
+     */
+    protected function addFacets(FacetSet $facetSet, $activeFacets)
+    {
+        $facetConfiguration = $this->getParameter('facets');
+
+        $filterQueries = [];
+        $facetCounter = $this->getFacetCounter($activeFacets);
+        foreach ($facetConfiguration as $facet) {
+            $facetSet->createFacetField($facet['title'])->setField($facet['field']);
+        }
+
+        if (count($activeFacets) > 0) {
+            foreach ($activeFacets as $activeFacet) {
+                $filterQuery = new FilterQuery();
+                foreach ($activeFacet as $itemKey => $item) {
+                    $filterQuery->setKey($itemKey.$this->getFacetCounter($activeFacets));
+                    $filterQuery->setQuery($itemKey.':"'.$item.'"');
+                }
+                $filterQueries[] = $filterQuery;
+                ++$facetCounter;
+            }
+        }
+
+        return $filterQueries;
     }
 }
