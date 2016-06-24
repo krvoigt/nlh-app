04d47b797f6c0d93e98eb899e9fbd11f920d39af
tschaef <tschaef1@gwdg.de>
2016-06-24 14:08:23 +0200

    Show coverflow as dock with fisheye hover FX
    On default, show only top-most section of pages. On approach, increase footer height until whole pages along with the scrollbar are visible. Additionally increase size of currently hovered and neighboring items.
    
    Behavior is still a bit shaky and should be improved.
    
    TODO: Make coverflow usable on touchscreens.

--- app/Resources/js/sections/coverflow.js
+++ app/Resources/js/sections/coverflow.js
@@ -12,5 +12,35 @@ $(function () {
             this.scrollLeft -= (delta * 100);
             event.preventDefault();
         });
+
+        var $container = $('.coverflow');
+        var $items = $('.coverflow_item');
+        var sizingFactor = 2;
+        var initialWidth = $items.first().width();
+        var initialHeight = $items.first().height();
+        var height = $coverflow.height() - 4;
+        var maxDistance = 200;
+        $coverflow.css({bottom: -height});
+        $('body').mousemove(function (e) {
+            verticalDistance = Math.max(0, $(window).height() - e.pageY - height * 3);
+            $coverflow.css({
+                bottom: verticalDistance < height ? -height + height * (height - verticalDistance) / height : -height
+            });
+
+            $items.each(function (i) {
+                distance = getMouseDistance($(this), e);
+                $(this).css({
+                    width: distance < maxDistance ? Math.round(initialWidth * (1 + (maxDistance - distance) / maxDistance)) : ''
+                });
+            });
+        });
+        // TODO: This doesn't seem to work.
+        $('body').trigger('mousemove');
+    }
+
+    function getMouseDistance(el, e) {
+        var x = Math.pow(e.pageX - (el.offset().left + (el.width() / 2)), 2);
+        var y = Math.max(3000, Math.pow(e.pageY - (el.offset().top + (el.height() / 2)), 2));
+        return Math.sqrt(x + y);
     }
 });
