898f5ba302ff296ad368aa3766145ce1bc904c0f
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2016-07-01 10:18:20 +0200

    Use compatible jquery version
    This replaces (a.k.a. downgrades) jquery to 2.2 which is compatible to
    all used libraries (i.e. panzoom).

--- gulpfile.js
+++ gulpfile.js
@@ -42,6 +42,7 @@ var paths = {
         'node_modules/leaflet/dist/leaflet.js',
         'node_modules/leaflet-iiif/leaflet-iiif.js',
         'node_modules/select2/dist/js/select2.min.js',
+        'node_modules/jquery.panzoom/dist/jquery.panzoom.js',
     ],
     vendorScriptDest: 'web/js',
 };
