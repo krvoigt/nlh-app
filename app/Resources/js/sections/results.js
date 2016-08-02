fa285d6a3a9bdf1e797fa806f978fa90654dbe5c
tschaef <tschaef1@gwdg.de>
2016-08-02 19:26:18 +0200

    Improve title shortening in results
    Check if a shortened title would actually gain space, and only shorten it if the metadata block is taller than the adjacent thumbnail. Add title tag to titles.

--- app/Resources/js/sections/results.js
+++ app/Resources/js/sections/results.js
@@ -1,13 +1,43 @@
 $(function () {
+    var titleLineHeight = parseInt($('.result_title').css('line-height'));
+
+    $('.result_thumbnail > img').each( function() {
+        var $parent = $(this).closest('.result_item');
+        if ($(this).height() > 0) {
+            $parent.adjustHeight(titleLineHeight);
+        } else {
+            $(this).one('load', function () {
+                $parent.adjustHeight(titleLineHeight);
+            });
+        }
+    });
+
     $(window).on('resize', function () {
-        $('.result_title').each(function () {
-            var hasLongTitle = $(this).find('.result_link').height() > $(this).height();
-            $(this).find('.result_title-toggle.-expand').toggle(hasLongTitle);
-        });
-    }).trigger('resize');
+        $('.result_item').adjustHeight(titleLineHeight);
+    });
 
     $('.result_title-toggle').click(function () {
         $(this).siblings('.result_title-toggle').addBack().toggle();
         $(this).closest('.result_title').toggleClass('-full');
     });
+});
+
+// TODO: Fix scope
+$.fn.extend({
+    adjustHeight: function (minHeight) {
+        return this.each(function () {
+            var $this = $(this);
+            var $title = $this.find('.result_title');
+            $title.height( '' ).removeClass('-cut');
+            var metadataHeight = $this.find('.result_metadata').height();
+            var thumbnailHeight = $this.find('.result_thumbnail > img').height();
+            var excessHeight = Math.floor(metadataHeight - thumbnailHeight);
+            if ( excessHeight > 0 ) {
+                $title.height( Math.max($title.height() - Math.ceil(excessHeight / minHeight) * minHeight, minHeight) ).addClass('-cut');
+                $this.find('.result_title-toggle.-expand').show();
+            } else {
+                $this.find('.result_title-toggle.-expand').hide();
+            }
+        });
+    },
 });
