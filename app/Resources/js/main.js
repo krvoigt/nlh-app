d516627fa087a2c4655e55e8e96d5874294b1c49
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2016-09-20 09:41:45 +0200

    Add sorting options to template
    This adds the sorting options date, relevance and import date to the
    search result list. When selection one of the radio values, the url
    parameter sort is added to the query string.
    
    Related: GDZ-316

--- app/Resources/js/main.js
+++ app/Resources/js/main.js
@@ -1,1 +1,18 @@
 $.fx.speeds._default = 250;
+
+$('.sort_item_input').change(function(){
+
+    var url = location.href,
+        paramName = 'sort',
+        paramValue = $(this).attr('id');
+    url = url.replace(/sort\=[a-z]*&?/, '');
+
+    if (url.indexOf('?') < 0) {
+        url += '?' + paramName + '=' + paramValue;
+    }
+    else {
+        url += '&' + paramName + '=' + paramValue;
+    }
+    history.pushState(null, null, url);
+    location.href = url;
+});
