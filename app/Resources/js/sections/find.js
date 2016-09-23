0ab6e618a9d4a48f2f2b704865ee20d2c285fb32
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2016-09-23 14:52:13 +0200

    Add more sorting options to the template
    This adds sorting directions ascending and descending for specified
    parameters to the template.
    
    Related: GDZ-316

--- app/Resources/js/sections/find.js
+++ app/Resources/js/sections/find.js
@@ -1,6 +1,8 @@
 $(function () {
     $('.find_sort-order').change(function(){
         var order = $(this).val();
-        setGetParameters({sort: order});
+        var sortby = _.split(order, '_');
+
+        setGetParameters({sort: sortby[0], direction: sortby[1]}, true);
     });
 });
