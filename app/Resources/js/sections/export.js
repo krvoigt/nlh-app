ff45116eb59fb3e91b23b7889376f62354d17829
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2016-09-28 15:28:40 +0200

    Add check if pdf download includes the complete document
    This adds a check whether the user requests the download of the complete
    document as pdf and redirects ist to the content server generated pdf.
    
    Resolves: GDZ-304
    Resolves: GDZ-305
    Related: GDZ-343

--- app/Resources/js/sections/export.js
+++ app/Resources/js/sections/export.js
@@ -8,12 +8,24 @@ var Export = {
     bindEvents: function () {
         $('.export_input.-page-start, .export_input.-page-end').change(this.checkPageRange.bind(this));
 
+        this.isCompleteDocument();
+        var that = this;
+
         $('.export_generate-pdf').click(function () {
             var ppn = $(this).data('ppn');
             // TODO: Sanitize. Add option to select arbitrary pages ("1, 3-6, 15").
             var physIDstart = parseInt($('#physIDstart').val());
             var physIDend = parseInt($('#physIDend').val());
-            DCPDF.generatePDF(ppn, physIDstart, physIDend);
+
+            if (that.isCompleteDocument()) {
+                var cacheUrlPrefix = 'http://gdz.sub.uni-goettingen.de/download/',
+                    identifier = ppn,
+                    element = 'LOG_0000';
+                window.location.href = cacheUrlPrefix + identifier + '/' + identifier + '___' + element + '.pdf';
+            } else {
+                DCPDF.generatePDF(ppn, physIDstart, physIDend);
+
+            }
         });
 
         $('.export_cancel, .export_reset').click(function () {
@@ -32,6 +44,14 @@ var Export = {
         $('.export_error.-page-range').toggle(isError);
         $('.export_generate-pdf').prop('disabled', isError);
     },
-}
+
+    isCompleteDocument: function () {
+        var $pageStart = $('.export_input.-page-start');
+        var $pageEnd = $('.export_input.-page-end');
+        var max = $('.export_page-count').text();
+
+        return parseInt($pageStart.val()) === 1 && parseInt($pageEnd.val()) === parseInt(max);
+    }
+};
 
 Export.init();
