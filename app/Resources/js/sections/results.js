55f1388176a4f12df4866760678c9457f9753a4a
tschaef <tschaef1@gwdg.de>
2016-10-20 18:54:01 +0200

    Improve search results script
    Refactor to match structure of other scripts, fix scope of title height function. Allow 2 lines instead of 1 in search result titles.

--- app/Resources/js/sections/results.js
+++ app/Resources/js/sections/results.js
@@ -1,43 +1,49 @@
-$(function () {
-    var titleLineHeight = parseInt($('.result_title').css('line-height'));
+var Results = {
+    minTitleHeight: parseInt($('.result_title').css('line-height')) * 2,
 
-    $('.result_thumbnail').each( function() {
-        var $parent = $(this).closest('.result_item');
-        if ($(this).height() > 0) {
-            $parent.adjustHeight(titleLineHeight);
-        } else {
-            $(this).one('load', function () {
-                $parent.adjustHeight(titleLineHeight);
-            });
-        }
-    });
+    init: function () {
+        var that = this;
 
-    $(window).on('resize', function () {
-        $('.result_item').adjustHeight(titleLineHeight);
-    });
+        $('.result_thumbnail').each( function() {
+            var $parent = $(this).closest('.result_item');
+            if ($(this).height() > 0) {
+                that.adjustTitleHeight($parent);
+            } else {
+                $(this).one('load', function () {
+                    that.adjustTitleHeight($parent);
+                });
+            }
+        });
 
-    $('.result_title-toggle').click(function () {
-        $(this).siblings('.result_title-toggle').addBack().toggle();
-        $(this).closest('.result_title').toggleClass('-full');
-    });
-});
+        this.bindEvents();
+    },
 
-// TODO: Fix scope
-$.fn.extend({
-    adjustHeight: function (minHeight) {
-        return this.each(function () {
+    bindEvents: function () {
+        $(window).on('resize', this.adjustTitleHeight.bind(this, $('.result_item')));
+
+        $('.result_title-toggle').click(function () {
+            $(this).siblings('.result_title-toggle').addBack().toggle();
+            $(this).closest('.result_title').toggleClass('-full');
+        });
+    },
+
+    adjustTitleHeight: function (item) {
+        var that = this;
+        return item.each(function () {
             var $this = $(this);
             var $title = $this.find('.result_title');
             $title.height( '' ).removeClass('-cut');
             var metadataHeight = $this.find('.result_metadata').height();
             var thumbnailHeight = $this.find('.result_thumbnail').height();
             var excessHeight = Math.floor(metadataHeight - thumbnailHeight);
-            if ( $title.height() > minHeight && excessHeight > 0 ) {
-                $title.height( Math.max($title.height() - Math.ceil(excessHeight / minHeight) * minHeight, minHeight) ).addClass('-cut');
+            if ( $title.height() > that.minTitleHeight && excessHeight > 0 ) {
+                $title.height( Math.max($title.height() - Math.ceil(excessHeight / that.minTitleHeight) * that.minTitleHeight, that.minTitleHeight) ).addClass('-cut');
                 $this.find('.result_title-toggle.-expand').show();
             } else {
                 $this.find('.result_title-toggle.-expand').hide();
             }
         });
     },
-});
+};
+
+Results.init();
