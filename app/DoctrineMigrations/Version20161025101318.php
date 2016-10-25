da3f21a2dbe745d136d40dd9c89fd39e8d83be34
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2016-10-25 14:34:40 +0200

    Fix migration

--- app/DoctrineMigrations/Version20161025101318.php
+++ app/DoctrineMigrations/Version20161025101318.php
@@ -15,9 +15,6 @@ class Version20161025101318 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
-
         $this->addSql('CREATE TEMPORARY TABLE __temp__user AS SELECT id, startIpAddress, endIpAddress, institution, product FROM user');
         $this->addSql('DROP TABLE user');
         $this->addSql('CREATE TABLE user (id INTEGER NOT NULL, startIpAddress INTEGER NOT NULL, endIpAddress INTEGER NOT NULL, institution VARCHAR(255) DEFAULT NULL COLLATE BINARY, product VARCHAR(30) NOT NULL COLLATE BINARY, PRIMARY KEY(id))');
@@ -30,9 +27,6 @@ class Version20161025101318 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
-
         $this->addSql('CREATE TEMPORARY TABLE __temp__user AS SELECT id, startIpAddress, endIpAddress, institution, product FROM user');
         $this->addSql('DROP TABLE user');
         $this->addSql('CREATE TABLE user (id INTEGER NOT NULL, startIpAddress VARCHAR(255) NOT NULL COLLATE BINARY, endIpAddress VARCHAR(255) NOT NULL COLLATE BINARY, institution VARCHAR(255) DEFAULT NULL, product VARCHAR(30) NOT NULL, PRIMARY KEY(id))');
