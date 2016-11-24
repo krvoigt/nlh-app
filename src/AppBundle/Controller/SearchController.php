ca648bf78befef4b1be8e389bee2dd000818880b
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2016-11-24 09:54:47 +0100

    Compose queries for advanced search
    Resolves: NLHOS-240

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -5,7 +5,6 @@ namespace AppBundle\Controller;
 use AppBundle\Form\Type\AdvancedSearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends Controller
@@ -23,15 +22,54 @@ class SearchController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $queryString = [];
+            $queryFragments = [];
 
             foreach ($form->getData() as $field => $datum) {
                 if ($datum !== null) {
-                    $queryString[] = vsprintf('%s:%s', [$field, $datum]);
+                    $queryFragments[] = vsprintf('%s:%s', [$field, $datum]);
                 }
             }
 
-            return new Response(implode(' ', $queryString));
+            $query = $this->get('subugoe_find.query_service')->composeQuery(implode(' ', $queryFragments));
+
+            $access = $request->get('access');
+            $searchService = $this->get('subugoe_find.search_service');
+            $queryService = $this->get('subugoe_find.query_service');
+            $client = $this->get('solarium.client');
+
+            $search = $searchService->getSearchEntity();
+            $select = $client->createSelect()->setQuery($query);
+
+            $activeFacets = $request->get('filter');
+            $queryService->addQueryFilters($select, $activeFacets);
+
+            $user = $this->get('authorization_service')->getAllowedProducts();
+            $products = $user->getProducts();
+            sort($products);
+
+            if ($access !== null) {
+                $select = $this->addFilterForAllowedProducts($products, $select);
+            }
+
+            $solrProducts = $this->get('document_service')->getAvailableProducts();
+
+            $fullAccess = ($products === $solrProducts) ? true : false;
+
+            $pagination = $searchService->getPagination($select);
+            $facets = $this->get('solarium.client')->select($select)->getFacetSet()->getFacets();
+            $facetCounter = $this->get('subugoe_find.query_service')->getFacetCounter($activeFacets);
+
+            return $this->render('SubugoeFindBundle:Default:index.html.twig', [
+                    'facets' => $facets,
+                    'facetCounter' => $facetCounter,
+                    'queryParams' => $request->get('filter') ?: [],
+                    'search' => $search,
+                    'pagination' => $pagination,
+                    'activeCollection' => $request->get('activeCollection'),
+                    'user' => $user,
+                    'access' => $access,
+                    'fullAccess' => $fullAccess,
+            ]);
         }
 
         return $this->render('search/advanced.html.twig', [
