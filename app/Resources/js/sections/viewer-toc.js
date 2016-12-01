6786af02652b4340134d57f7e1ad495f6c2df406
tschaef <tschaef1@gwdg.de>
2016-12-01 18:05:57 +0100

    Revise TOC, hide PDF button
    Change links to block format, hide PDF button until chapter PDFs are available.
    
    Related: NLHOS-219

--- app/Resources/js/sections/viewer-toc.js
+++ app/Resources/js/sections/viewer-toc.js
@@ -9,7 +9,6 @@ var ViewerToc = {
         $.get(window.location.origin + window.location.pathname + '/toc/', function (data) {
             $('.toc_content', this.container).html(data);
             that.controls.tocLink = $('.toc_link, .toc_page-number', this.container),
-            that.controls.tocPdf = $('.toc_pdf', this.container),
             that.bindEvents();
         });
         this.isInited = true;
@@ -17,14 +16,7 @@ var ViewerToc = {
 
     bindEvents: function () {
         this.controls.tocLink.click(function (e) {
-            setGetParameters({page: $(this).data('page')}, true);
-            return false;
-        });
-
-        this.controls.tocPdf.click(function () {
-            Viewer.controls.togglePanel.filter('[data-target=export]:not(.-active)').click();
-            $('#physIDstart').val( $(this).data('start') );
-            $('#physIDend').val( $(this).data('end') );
+            window.location = $(this).attr('href') + '&origin=' + encodeURIComponent($('#origin').attr('href')) + window.location.hash;
             return false;
         });
     },
